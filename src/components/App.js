1bf25f4b7cd45381167449cff55e7c8c212137a2
oscar kiprop chemonges <oscar.chemonges@student.moringaschool.com>
2022-07-14 14:55:15 +0300

    crete Link Componet

--- src/components/App.js
+++ src/components/App.js
@@ -5,16 +5,24 @@ import About from "./About";
 
 // pass this data down as props to the child component(s) that need it!
 import user from "../data/user";
+import { unstable_renderSubtreeIntoContainer } from "react-dom";
 console.log(user);
 
 function App() {
   return (
     <div>
       <NavBar />
-      <Home />
-      <About />
+      <Home name={user.name} city={user.city} color={user.color}/>
+      <About bio={user.bio} links={user.links} />
     </div>
   );
 }
+// function Home(user){
+//   return (
+//     <div>
+//       {user.name} {user.city} {user.color}
+//     </div>
+//   )
+// }
 
 export default App;
